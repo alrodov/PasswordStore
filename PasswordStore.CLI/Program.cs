279b1ad30619727e8020ac7614c4c9caba3effab
Alex <alexrodov22@gmail.com>
2021-02-09 21:44:05 +0300

    Initial commit

---
+++ PasswordStore.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasswordStore.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
